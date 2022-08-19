d1a326e5dd3ef9a7d4de2065697c4bc8a65061d7
Alan Jui <alanjui.1960@gmail.com>
2022-08-19 22:12:12 +0800

    nvim-dap-ui 新版為配合 nvim 0.7 版之變更，致使原先的 dapui.setup({}) 設定失效。故變更已有之設定，以改善此異常問題。

--- lua/dap-debug/init.lua
+++ lua/dap-debug/init.lua
@@ -59,7 +59,59 @@ local function configure_debug_ui()
 
     -- 設定「除錯器」的「使用者介面」在「右側」顯示
     dapui.setup({
-        layouts = { position = 'right' }
+        icons = { expanded = "▾", collapsed = "▸" },
+        mappings = {
+            -- Use a table to apply multiple mappings
+            expand = { "<CR>", "<2-LeftMouse>" },
+            open = "o",
+            remove = "d",
+            edit = "e",
+            repl = "r",
+            toggle = "t",
+        },
+        -- Expand lines larger than the window
+        -- Requires >= 0.7
+        expand_lines = vim.fn.has("nvim-0.7"),
+        -- Layouts define sections of the screen to place windows.
+        -- The position can be "left", "right", "top" or "bottom".
+        -- The size specifies the height/width depending on position. It can be an Int
+        -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+        -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+        -- Elements are the elements shown in the layout (in order).
+        -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+        layouts = {
+            {
+                elements = {
+                    -- Elements can be strings or table with id and size keys.
+                    { id = "scopes", size = 0.25 },
+                    "breakpoints",
+                    "stacks",
+                    "watches",
+                },
+                size = 40, -- 40 columns
+                position = "left",
+            },
+            {
+                elements = {
+                    "repl",
+                    "console",
+                },
+                size = 0.25, -- 25% of total lines
+                position = "bottom",
+            },
+        },
+        floating = {
+            max_height = nil, -- These can be integers or a float between 0 and 1.
+            max_width = nil, -- Floats will be treated as percentage of your screen.
+            border = "single", -- Border style. Can be "single", "double" or "rounded"
+            mappings = {
+                close = { "q", "<Esc>" },
+            },
+        },
+        windows = { indent = 1 },
+        render = {
+            max_type_length = nil, -- Can be integer or nil.
+        }
     })
 
     -- 完成「初始作業」後，便顯示使用者介面
@@ -111,11 +163,11 @@ configure_debug_ui()
 -- DAP 操作之各項「操作指令」，於 which_key 中之 "debug" 指令選單中設定。
 -- 設定【快捷鍵】
 vim.cmd([[
-    nnoremap <silent> <F4>  <cmd>lua require'dap'.repl.open()<CR>
-    nnoremap <silent> <F5>  <cmd>lua require'dap'.continue()<CR>
-    nnoremap <silent> <F9>  <cmd>lua require'dap'.toggle_breakpoint()<CR>
-    nnoremap <silent> <F10> <cmd>lua require'dap'.step_over()<CR>
-    nnoremap <silent> <F11> <cmd>lua require'dap'.step_into()<CR>
-    nnoremap <silent> <F12> <cmd>lua require'dap'.step_out()<CR>
+nnoremap <silent> <F4>  <cmd>lua require'dap'.repl.open()<CR>
+nnoremap <silent> <F5>  <cmd>lua require'dap'.continue()<CR>
+nnoremap <silent> <F9>  <cmd>lua require'dap'.toggle_breakpoint()<CR>
+nnoremap <silent> <F10> <cmd>lua require'dap'.step_over()<CR>
+nnoremap <silent> <F11> <cmd>lua require'dap'.step_into()<CR>
+nnoremap <silent> <F12> <cmd>lua require'dap'.step_out()<CR>
 ]])
 
