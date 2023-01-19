df22b64bfde09cf8452337d36f495a065c362955
居正中 <AlanJui.1960@gmail.com>
2023-01-19 10:25:33 +0800

    修訂 trim.nvim 擴充套件的載入方式。

--- lua/plugins-rc/trim-nvim.lua
+++ lua/plugins-rc/trim-nvim.lua
@@ -1,18 +1,18 @@
 -- This plugin trims trailing whitespace and lines.
-local status, trim_nvim = pcall(require, 'trim')
-if not trim_nvim then
-    return
+local ok, trim_nvim = pcall(require, "trim")
+if not ok then
+	return
 end
 
 trim_nvim.setup({
-    trim_trailing = true,
-    trim_last_line = true,
-    trim_first_line = true,
-    disable = {
-        "markdown",
-    },
-    -- if you want to remove multiple blank lines
-    -- patterns = {
-    --   [[%s/\(\n\n\)\n\+/\1/]],   -- replace multiple blank lines with a single line
-    -- },
+	trim_trailing = true,
+	trim_last_line = true,
+	trim_first_line = true,
+	disable = {
+		"markdown",
+	},
+	-- if you want to remove multiple blank lines
+	-- patterns = {
+	--   [[%s/\(\n\n\)\n\+/\1/]],   -- replace multiple blank lines with a single line
+	-- },
 })
