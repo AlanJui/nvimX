193005a40c03aabcf756a8f0830879e8a42746d9
居正中 <AlanJui.1960@gmail.com>
2023-02-17 11:12:46 +0800

    增加對 File Type 為 htmldjango 的支援

--- after/ftplugin/html.vim
+++ after/ftplugin/html.vim
@@ -1,6 +1,6 @@
 " Tabs
-set ts=2
-set sw=2
+set ts=4
+set sw=4
 
 let b:surround_{char2nr("v")} = "{{ \r }}"
 let b:surround_{char2nr("{")} = "{{ \r }}"
@@ -14,39 +14,39 @@ let b:surround_{char2nr("c")} = "{% comment %}\r{% endcomment %}"
 ""--------------------------------------------------------------
 "" Django
 
-"autocmd FileType htmldjango setlocal shiftwidth=4 tabstop=4 softtabstop=4
-"autocmd FileType htmldjango inoremap {{ {{  }}<left><left><left>
-"autocmd FileType htmldjango inoremap {% {%  %}<left><left><left>
-"autocmd FileType htmldjango inoremap {# {#  #}<left><left><left>
+autocmd FileType htmldjango setlocal shiftwidth=4 tabstop=4 softtabstop=4
+autocmd FileType htmldjango inoremap {{ {{  }}<left><left><left>
+autocmd FileType htmldjango inoremap {% {%  %}<left><left><left>
+autocmd FileType htmldjango inoremap {# {#  #}<left><left><left>
 
-"" 判別是否適用 htmldjango 檔案格式
-"" === To enable filetype for htmldjango ===
-"" 判別是否適用 django-html 檔案格式
-"" 可能選項：
-""  (1) {% extends
-""  (2) {% block
-""  (3) {% load
-""  (4) {#
-"" Regex Express 判別式：
-""  (1) {%\s*\(extends\|block\|load\)\>\|{#\s\+
-""  (2) {%\|{{\|{#
+" 判別是否適用 htmldjango 檔案格式
+" === To enable filetype for htmldjango ===
+" 判別是否適用 django-html 檔案格式
+" 可能選項：
+"  (1) {% extends
+"  (2) {% block
+"  (3) {% load
+"  (4) {#
+" Regex Express 判別式：
+"  (1) {%\s*\(extends\|block\|load\)\>\|{#\s\+
+"  (2) {%\|{{\|{#
 
 
-"augroup filetypedetect
-"  " removes current htmldjango detection located at $VIMRUNTIME/filetype.vim
-"  autocmd! BufNewFile,BufRead *.html
-"  autocmd  BufNewFile,BufRead *.html call FThtml()
+augroup filetypedetect
+  " removes current htmldjango detection located at $VIMRUNTIME/filetype.vim
+  autocmd! BufNewFile,BufRead *.html
+  autocmd  BufNewFile,BufRead *.html call FThtml()
 
-"  func! FThtml()
-"    let n = 1
-"    while n < 10 && n < line("$")
-"        if getline(n) =~ '{%\|{{\|{#'
-"        setf htmldjango
-"        return
-"      endif
-"      let n = n + 1
-"    endwhile
-"    "setf html
-"    setf html
-"  endfunc
-"augroup END
+  func! FThtml()
+    let n = 1
+    while n < 10 && n < line("$")
+        if getline(n) =~ '{%\|{{\|{#'
+        setf htmldjango
+        return
+      endif
+      let n = n + 1
+    endwhile
+    "setf html
+    setf html
+  endfunc
+augroup END
--- lua/plugins-rc/nvim-treesitter.lua
+++ lua/plugins-rc/nvim-treesitter.lua
@@ -5,72 +5,73 @@
 -- import nvim-treesitter plugin safely
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-	return
+    return
 end
 
 treesitter.setup({
-	-- ensure these language parsers are installed
-	-- A list of parser names, or "all"
-	ensure_installed = {
-		"lua",
-		"vim",
-		"python",
-		"json",
-		"javascript",
-		"typescript",
-		"tsx",
-		"css",
-		"markdown",
-		"svelte",
-		"graphql",
-		"yaml",
-		"toml",
-		"html",
-		"dockerfile",
-		"gitignore",
-		"comment",
-		"latex",
-		"bash",
-		"c",
-		"cmake",
-		"go",
-		"rust",
-	},
-	-- auto install above language parsers
-	auto_install = true,
-	-- enable indentation
-	indent = { enable = true },
-	-- enable autotagging (w/ nvim-ts-autotag plugin)
-	autotag = { enable = true },
-	-- enable syntax highlighting
-	highlight = {
-		-- `false` will disable the whole extension
-		enable = true,
-		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-		-- the name of the parser)
+    -- ensure these language parsers are installed
+    -- A list of parser names, or "all"
+    ensure_installed = {
+        "lua",
+        "vim",
+        "python",
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "css",
+        "markdown",
+        "svelte",
+        "graphql",
+        "yaml",
+        "toml",
+        "html",
+        "htmldjango",
+        "dockerfile",
+        "gitignore",
+        "comment",
+        "latex",
+        "bash",
+        "c",
+        "cmake",
+        "go",
+        "rust",
+    },
+    -- auto install above language parsers
+    auto_install = true,
+    -- enable indentation
+    indent = { enable = true },
+    -- enable autotagging (w/ nvim-ts-autotag plugin)
+    autotag = { enable = true },
+    -- enable syntax highlighting
+    highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
+        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+        -- the name of the parser)
 
-		-- list of language that will be disabled
-		-- disable = { "c", "rust" },
+        -- list of language that will be disabled
+        -- disable = { "c", "rust" },
 
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
-		-- additional_vim_regex_highlighting = false,
-	},
-	autopairs = { enable = true },
-	context_commentstring = {
-		enable = true,
-		config = {
-			css = "// %s",
-			javascript = {
-				__default = "// %s",
-				jsx_element = "{/* %s */}",
-				jsx_fragment = "{/* %s */}",
-				jsx_attribute = "// %s",
-				comment = "// %s",
-			},
-		},
-	},
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        -- additional_vim_regex_highlighting = false,
+    },
+    autopairs = { enable = true },
+    context_commentstring = {
+        enable = true,
+        config = {
+            css = "// %s",
+            javascript = {
+                __default = "// %s",
+                jsx_element = "{/* %s */}",
+                jsx_fragment = "{/* %s */}",
+                jsx_attribute = "// %s",
+                comment = "// %s",
+            },
+        },
+    },
 })
