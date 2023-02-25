dd831e4d7354993b2062fe036b163694467af6ee
居正中 <AlanJui.1960@gmail.com>
2023-02-25 10:58:12 +0800

    為 cmp.nvim 加上「框線」。

--- lua/lsp/lsp-autocmp.lua
+++ lua/lsp/lsp-autocmp.lua
@@ -58,7 +58,10 @@ cmp.setup({
 			luasnip.lsp_expand(args.body)
 		end,
 	},
-	window = { documentation = cmp.config.window.bordered() },
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-y>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
