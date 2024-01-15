5247d94ddeee633329fd370c1448dfe91e8d9071
Alan Jui <AlanJui.1960@gmail.com>
2024-01-15 11:09:38 +0800

    新增「操作指引」及「使用指令」。

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,6 +1,22 @@
+-- Trouble comes with the following commands:
+--
+-- Trouble [mode]: open the list
+-- TroubleClose [mode]: close the list
+-- TroubleToggle [mode]: toggle the list
+-- TroubleRefresh: manually refresh the active list
+--
+-- Modes:
+--  * document_diagnostics: document diagnostics from the builtin LSP client
+--  * workspace_diagnostics: workspace diagnostics from the builtin LSP client
+--  * lsp_references: references of the word under the cursor from the builtin LSP client
+--  * lsp_definitions: definitions of the word under the cursor from the builtin LSP client
+--  * lsp_type_definitions: type definitions of the word under the cursor from the builtin LSP client
+--  * quickfix: quickfix items
+--  * loclist: items from the window's location list
 return {
   "folke/trouble.nvim",
-  cmd = { "TroubleToggle", "Trouble" },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  cmd = { "TroubleToggle", "Trouble", "TroubleClose", "TroubleRefresh" },
   opts = { use_diagnostic_signs = true },
   keys = {
     { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
