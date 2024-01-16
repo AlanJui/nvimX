88283dfb120286d02ed58df1a4a731662d6f8c19
Alan Jui <AlanJui.1960@gmail.com>
2024-01-16 18:03:15 +0800

    變更 Python/Node.js Provider 的設定方式。

--- init.lua
+++ init.lua
@@ -3,11 +3,15 @@
 ------------------------------------------------
 require("config.options")
 require("globals")
-local nvim_config = _G.GetConfig()
+
+local home_dir = os.getenv "HOME"
+local PYTHON_VERSION = "3.12.1"
+
 vim.g.loaded_python2_provider = 0
 vim.g.loaded_python3_provider = 1
-vim.g.python3_host_prog = nvim_config["python"]["nvim_binary"]
-vim.g.node_host_prog = nvim_config["nodejs"]["node_host_prog"]
+vim.g.python3_host_prog = home_dir .. "/.pyenv/versions/" .. PYTHON_VERSION .. "/bin/python"
+
+vim.g.node_host_prog = home_dir .. "/n/bin/neovim-node-host"
 vim.g.loaded_python_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
