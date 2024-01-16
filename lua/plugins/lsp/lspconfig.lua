b34a0adb314cd6c20deaa58c0c0db41ca25628c2
Alan Jui <AlanJui.1960@gmail.com>
2024-01-16 18:06:01 +0800

    將 lua LSP Server 之設定工作，改用 mason-lspconfig.nvim 。

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -76,25 +76,25 @@ return {
     end
 
     -- configure lua server (with special settings)
-    lspconfig["lua_ls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = { -- custom settings for lua
-        Lua = {
-          -- make the language server recognize "vim" global
-          diagnostics = {
-            globals = { "vim", "hs" },
-          },
-          workspace = {
-            -- make language server aware of runtime files
-            library = {
-              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-              [vim.fn.stdpath("config") .. "/lua"] = true,
-            },
-          },
-        },
-      },
-    })
+    -- lspconfig["lua_ls"].setup({
+    --   capabilities = capabilities,
+    --   on_attach = on_attach,
+    --   settings = { -- custom settings for lua
+    --     Lua = {
+    --       -- make the language server recognize "vim" global
+    --       diagnostics = {
+    --         globals = { "vim", "hs" },
+    --       },
+    --       workspace = {
+    --         -- make language server aware of runtime files
+    --         library = {
+    --           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+    --           [vim.fn.stdpath("config") .. "/lua"] = true,
+    --         },
+    --       },
+    --     },
+    --   },
+    -- })
 
     -- configure typescript server with plugin
     lspconfig["tsserver"].setup({
