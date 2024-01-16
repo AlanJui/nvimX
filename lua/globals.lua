b58c26b5783610a50a4f22b00cc06af7c4b53633
Alan Jui <AlanJui.1960@gmail.com>
2024-01-16 18:01:38 +0800

    調整 Global Functions/Variables

--- lua/globals.lua
+++ lua/globals.lua
@@ -2,62 +2,6 @@ vim.g.mapleader = " "
 
 local path_sep = vim.loop.os_uname().version:match("Windows") and "\\" or "/"
 
-local function file_exists(name)
-  local f = io.open(name, "r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
-end
-
-local function which_os()
-  local system_name
-
-  if vim.fn.has("mac") == 1 then
-    system_name = "macOS"
-  elseif vim.fn.has("unix") == 1 then
-    system_name = "Linux"
-  elseif vim.fn.has("win32") == 1 then
-    system_name = "Windows"
-  else
-    system_name = ""
-  end
-
-  return system_name
-end
-
-local function tprint(tbl, indent)
-  if not indent then indent = 0 end
-  local toprint = string.rep(" ", indent) .. "{\n"
-  indent = indent + 2
-  for k, v in pairs(tbl) do
-    toprint = toprint .. string.rep(" ", indent)
-    if type(k) == "number" then
-      toprint = toprint .. "[" .. k .. "] = "
-    elseif type(k) == "string" then
-      toprint = toprint .. k .. "= "
-    end
-    if type(v) == "number" then
-      toprint = toprint .. v .. ",\n"
-    elseif type(v) == "string" then
-      toprint = toprint .. '"' .. v .. '",\n'
-    elseif type(v) == "table" then
-      toprint = toprint .. tprint(v, indent + 2) .. ",\n"
-    else
-      toprint = toprint .. '"' .. tostring(v) .. '",\n'
-    end
-  end
-  toprint = toprint .. string.rep(" ", indent - 2) .. "}"
-  return toprint
-end
-
------------------------------------------------------------
--- Global Functions
------------------------------------------------------------
-_G.DEBUG = os.getenv("DEBUG")
-local os_sys = which_os()
 local nvim_name = os.getenv("MY_NVIM") or vim.g.my_nvim or "nvim"
 local home_dir = os.getenv("HOME")
 -- local config_dir = home_dir .. "/.config/" .. nvim_name
@@ -96,6 +40,77 @@ local LSP_SERVERS = {
 }
 
 -----------------------------------------------------------
+-- Global Variables
+-----------------------------------------------------------
+_G.DEBUG = os.getenv("DEBUG")
+
+-- Python environment for NeoVim
+local PYENV_ROOT_PATH = home_dir .. "/.pyenv/versions/"
+local PYTHON_VERSION = "3.12.1"
+-- local PYTHON_VENV = "venv-" .. PYTHON_VERSION
+-- local PYTHON_BINARY = PYENV_ROOT_PATH .. PYTHON_VERSION .. "/envs/" .. PYTHON_VENV .. "/bin/python"
+local debugpy_path = runtime_dir .. "/mason/packages/debugpy/venv/bin/python"
+PYTHON_BINARY = home_dir .. "/.pyenv/versions/" .. PYTHON_VERSION .. "/bin/python"
+
+-----------------------------------------------------------
+-- Local Functions
+-----------------------------------------------------------
+local function which_os()
+  local system_name
+
+  if vim.fn.has("mac") == 1 then
+    system_name = "macOS"
+  elseif vim.fn.has("unix") == 1 then
+    system_name = "Linux"
+  elseif vim.fn.has("win32") == 1 then
+    system_name = "Windows"
+  else
+    system_name = ""
+  end
+
+  return system_name
+end
+
+local function file_exists(name)
+  local f = io.open(name, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
+local function tprint(tbl, indent)
+  if not indent then
+    indent = 0
+  end
+  local toprint = string.rep(" ", indent) .. "{\n"
+  indent = indent + 2
+  for k, v in pairs(tbl) do
+    toprint = toprint .. string.rep(" ", indent)
+    if type(k) == "number" then
+      toprint = toprint .. "[" .. k .. "] = "
+    elseif type(k) == "string" then
+      toprint = toprint .. k .. "= "
+    end
+    if type(v) == "number" then
+      toprint = toprint .. v .. ",\n"
+    elseif type(v) == "string" then
+      toprint = toprint .. '"' .. v .. '",\n'
+    elseif type(v) == "table" then
+      toprint = toprint .. tprint(v, indent + 2) .. ",\n"
+    else
+      toprint = toprint .. '"' .. tostring(v) .. '",\n'
+    end
+  end
+  toprint = toprint .. string.rep(" ", indent - 2) .. "}"
+  return toprint
+end
+
+-----------------------------------------------------------
+-- Global Functions
+-----------------------------------------------------------
 -- 功能：檢查目前的「工作目錄」中是否有 pyproject.toml 檔案，
 -- 若有，表：此為目錄為 Python 專案目錄。若目錄下尚有 .venv
 -- 子目錄，則再於 .venv 目錄下找 python 直譯器之路徑；若無
@@ -108,7 +123,6 @@ local LSP_SERVERS = {
 --
 -- 第二返回值，資料型態為：string ，表 Python Virtual Environment
 -- 的 Python 路徑。當第一返回值為 false 時，則第二返回值當為 Null 。
------------------------------------------------------------
 function _G.check_python_project()
   -- 取得當前檔案路徑：vim.fn.expand('%:p') 取得當前檔案的絕對路徑
   -- local current_file_path = vim.fn.expand("%:p")
@@ -132,7 +146,9 @@ function _G.check_python_project()
       python_path = venv_dir_path .. "/bin/python"
     else
       local pyenv_virtual_env = os.getenv("VIRTUAL_ENV") or ""
-      if pyenv_virtual_env ~= "" then python_path = pyenv_virtual_env .. "/bin/python" end
+      if pyenv_virtual_env ~= "" then
+        python_path = pyenv_virtual_env .. "/bin/python"
+      end
     end
 
     -- 返回結果
@@ -144,19 +160,13 @@ end
 local is_python_project, venv_python_path = check_python_project()
 
 -----------------------------------------------------------
--- Python environment for NeoVim
------------------------------------------------------------
-local PYENV_ROOT_PATH = home_dir .. "/.pyenv/versions/"
-local PYTHON_VERSION = "3.10.6"
-local PYTHON_VENV = "venv-" .. PYTHON_VERSION
-local PYTHON_BINARY = PYENV_ROOT_PATH .. PYTHON_VERSION .. "/envs/" .. PYTHON_VENV .. "/bin/python"
-local debugpy_path = runtime_dir .. "/mason/packages/debugpy/venv/bin/python"
------------------------------------------------------------
 -- Python environment for Project
 -----------------------------------------------------------
 local pyenv_python_path = ""
 local pyenv_virtual_env = os.getenv("VIRTUAL_ENV") or ""
-if pyenv_virtual_env ~= "" then pyenv_python_path = pyenv_virtual_env .. "/bin/python" end
+if pyenv_virtual_env ~= "" then
+  pyenv_python_path = pyenv_virtual_env .. "/bin/python"
+end
 local workspace_folder = vim.fn.getcwd()
 
 local get_venv_python_path = function()
@@ -165,10 +175,10 @@ local get_venv_python_path = function()
   -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
   if vim.fn.executable(pyenv_python_path) then
     return pyenv_python_path
-  elseif vim.fn.executable(workspace_folder .. "/venv/bin/python") == 1 then
-    return workspace_folder .. "/venv/bin/python"
   elseif vim.fn.executable(workspace_folder .. "/.venv/bin/python") == 1 then
     return workspace_folder .. "/.venv/bin/python"
+  elseif vim.fn.executable(workspace_folder .. "/venv/bin/python") == 1 then
+    return workspace_folder .. "/venv/bin/python"
   else
     return "/usr/bin/python"
   end
@@ -177,7 +187,7 @@ end
 -----------------------------------------------------------
 function _G.GetConfig()
   return {
-    os = os_sys,
+    os = which_os(),
     home = home_dir,
     nvim = nvim_name,
     config = config_dir,
@@ -284,13 +294,17 @@ function _G.print_table(node)
         end
       else
         -- close the table
-        if cur_index == size then output_str = output_str .. "\n" .. string.rep("\t", depth - 1) .. "}" end
+        if cur_index == size then
+          output_str = output_str .. "\n" .. string.rep("\t", depth - 1) .. "}"
+        end
       end
 
       cur_index = cur_index + 1
     end
 
-    if size == 0 then output_str = output_str .. "\n" .. string.rep("\t", depth - 1) .. "}" end
+    if size == 0 then
+      output_str = output_str .. "\n" .. string.rep("\t", depth - 1) .. "}"
+    end
 
     if #stack > 0 then
       node = stack[#stack]
@@ -314,9 +328,13 @@ function _G.DumpTable(table)
   end
 end
 
-function _G.PrintTableWithIndent(table, indent_size) print(tprint(table, indent_size)) end
+function _G.PrintTableWithIndent(table, indent_size)
+  print(tprint(table, indent_size))
+end
 
-function _G.PrintTable(table) _G.print_table(table) end
+function _G.PrintTable(table)
+  _G.print_table(table)
+end
 
 function _G.IsDirNotExist(dir_path)
   if file_exists(dir_path) == true then
@@ -336,11 +354,15 @@ end
 
 function _G.Is_packer_nvim_installed()
   local installed = false
-  if vim.fn.empty(vim.fn.glob(install_path)) == 0 then installed = true end
+  if vim.fn.empty(vim.fn.glob(install_path)) == 0 then
+    installed = true
+  end
   return installed
 end
 
-function _G.is_empty(str) return str == nil or str == "" end
+function _G.is_empty(str)
+  return str == nil or str == ""
+end
 
 -----------------------------------------------------------------------------
 -- Exampe: JoinPaths("a", "b", "c") => "a/b/c"
@@ -350,19 +372,29 @@ function _G.JoinPaths(...)
   return result
 end
 
-function _G.is_git_dir() return os.execute("git rev-parse --is-inside-work-tree >> /dev/null 2>&1") end
+function _G.is_git_dir()
+  return os.execute("git rev-parse --is-inside-work-tree >> /dev/null 2>&1")
+end
 
-function _G.get_home_dir() return os.getenv("HOME") end
+function _G.get_home_dir()
+  return os.getenv("HOME")
+end
 
-function _G.GetHomeDir() return os.getenv("HOME") end
+function _G.GetHomeDir()
+  return os.getenv("HOME")
+end
 
 function _G.get_snippets_dir()
   return snippets_path
 end
 
-function _G.P(cmd) print(vim.inspect(cmd)) end
+function _G.P(cmd)
+  print(vim.inspect(cmd))
+end
 
-function _G.get_rtp() print(string.format("rtp = %s", vim.opt.rtp["_value"])) end
+function _G.get_rtp()
+  print(string.format("rtp = %s", vim.opt.rtp["_value"]))
+end
 
 function _G.safe_require(module)
   local ok, result = pcall(require, module)
