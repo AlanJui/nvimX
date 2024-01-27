665367d7f25b44185bea54d481d81c2c5c266d8f
Alan Jui <alanjui.1960@gmail.com>
2024-01-27 12:18:03 +0800

    為避免 vim.schedule 引發之問題，更改 lua-language-server 設定

--- lua/plugins/lsp/configs/lua_ls.lua
+++ lua/plugins/lsp/configs/lua_ls.lua
@@ -14,18 +14,4 @@ return {
       },
     },
   },
-  -- -- mason = false, -- set to false if you don't want this server to be installed with mason
-  -- settings = {
-  --   Lua = {
-  --     workspace = {
-  --       checkThirdParty = false,
-  --     },
-  --     completion = {
-  --       callSnippet = "Replace",
-  --     },
-  --     diagnostics = {
-  --       globals = { "vim", "hs" },
-  --     },
-  --   },
-  -- },
 }
